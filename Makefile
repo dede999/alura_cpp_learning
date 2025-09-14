460f27ea4f449c54da8f5b99fb380869fc68b0f5
André Luiz <andreluizsa00@gmail.com>
2025-09-13 23:12:52 -0300

    refactor: reorganize build output and add accounts program
    - Move all executables to bin/ directory for better organization (more
        than this, this directory existence makes it easier to place the
        executables in the .gitignore)
    - Update Makefile to output to bin/ and add new accounts target
    - Add new accounts.cpp program for account management

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 %: %.cpp
-	g++ $< -O3 -o $@
+	g++ $< -O3 -o bin/$@
 
 play_guessing_game: guessing_game
-	./guessing_game
+	./bin/guessing_game
 
 forca: forca.cpp headers/forca/*.cpp
-	g++ forca.cpp headers/forca/*.cpp -O3 -o forca
+	g++ forca.cpp headers/forca/*.cpp -O3 -o bin/forca
+
+accounts: accounts.cpp headers/accounts/*.cpp
+	g++ accounts.cpp headers/accounts/*.cpp -O3 -o bin/accounts
 
 play_forca: forca
-	./forca
+	./bin/forca
+
+see_accounts: accounts
+	./bin/accounts
---
+++ accounts.cpp
@@ -0,0 +1,32 @@
+#include "headers/accounts/main.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  Account acc;
+  acc.holder_name = "Andre";
+  acc.holder_soc_sec_number = "123.456.789-00";
+  acc.amount = 1000;
+
+  Account acc2;
+  acc2.holder_name = "Joao";
+  acc2.holder_soc_sec_number = "987.654.321-00";
+  acc2.amount = 500;
+
+  if (transfer(acc, acc2, 250)) {
+    cout << "Transfer successful" << endl;
+    cout << "Account holder " << acc2.holder_name << " received 250 dollars from " << acc.holder_name << endl;
+  } else {
+    cout << "Transfer failed" << endl;
+  }
+
+  if (transfer(acc2, acc, 800)) {
+    cout << "Transfer successful" << endl;
+    cout << "Account holder " << acc.holder_name << " received 800 dollars from " << acc2.holder_name << endl;
+  } else {
+    cout << "Transfer failed" << endl;
+  }
+
+  return 0;
+}
